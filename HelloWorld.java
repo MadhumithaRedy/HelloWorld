da04533d2244c53d56ec72e93b2db9d942b640d2
MadhumithaRedy <reddymadhumitha9550@gmail.com>
2023-08-16 15:10:36 +0000

    second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World!" 0);
+        System.out.println("Hello, World! 1");
+	 System.out.println("Hello, World! 2");
+	  System.out.println("Hello, World! 3");
     }
 }
